c1f17448578ab1355c4aa9ce04c8b71571142fa9
Alexey Semenkov <sealexey@gmail.com>
2017-03-16 14:51:10 +0300

    Upgrade vrsion (#12)
    * Upgrade vrsion
    
    * WALK-19: upgraded to woody 1.1.1
    
    * Update pom.xml
    
    Changed version to 1.0.6 (1.0.5 was previous)

--- src/main/java/com/rbkmoney/eventstock/client/poll/PollingWorker.java
+++ src/main/java/com/rbkmoney/eventstock/client/poll/PollingWorker.java
@@ -3,7 +3,7 @@ package com.rbkmoney.eventstock.client.poll;
 import com.rbkmoney.damsel.event_stock.DatasetTooBig;
 import com.rbkmoney.damsel.event_stock.StockEvent;
 import com.rbkmoney.eventstock.client.*;
-import com.rbkmoney.woody.api.concurrent.WRunnable;
+import com.rbkmoney.woody.api.flow.WFlow;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,7 +22,8 @@ class PollingWorker implements Runnable {
     private final PollingConfig<StockEvent> pollingConfig;
     private final ServiceAdapter<StockEvent, EventConstraint> serviceAdapter;
     private final String subscriptionKey;
-    private final Runnable wRunnable;
+    private final Runnable task = () -> runPolling();
+    private final WFlow wFlow = new WFlow();
     private RangeWalker<? extends Comparable, ? extends EventRange> rangeWalker;
     private boolean running = true;
     private int pollingLimit;
@@ -32,140 +33,141 @@ class PollingWorker implements Runnable {
         this.pollingConfig = pollingConfig;
         this.serviceAdapter = serviceAdapter;
         this.subscriptionKey = subscriptionKey;
-        this.wRunnable = WRunnable.create(() -> runPolling());
         this.pollingLimit = pollingConfig.getMaxQuerySize();
     }
 
     @Override
     public void run() {
-        wRunnable.run();
+        task.run();
     }
 
     private synchronized void runPolling() {
-        try {
-            LogSupport.setSubscriptionKey(subscriptionKey);
-            boolean exhausted = false;
-            boolean suspended = false;
-
+        wFlow.createServiceFork(() -> {
             try {
-                if (rangeWalker == null) {
-                    log.debug("Range is not initialized, init range");
-                    rangeWalker = initRange(pollingConfig.getEventFilter().getEventConstraint());
+                LogSupport.setSubscriptionKey(subscriptionKey);
+                boolean exhausted = false;
+                boolean suspended = false;
+
+                try {
                     if (rangeWalker == null) {
-                        log.debug("Range walker is not initialized, pause");
-                        return;
-                    } else {
-                        log.debug("Range initialized: {}", rangeWalker);
-                    }
-                }
-
-                while (running && !exhausted && !suspended && !Thread.currentThread().isInterrupted()) {
-                    if (rangeWalker.isRangeOver()) {
-                        log.debug("Range is over: {}", rangeWalker);
-                        exhausted = true;
-                        continue;
-                    }
-
-                    EventConstraint currentConstraint = new EventConstraint(rangeWalker.getWalkingRange());
-
-                    log.debug("Tying to get event range, constraint: {}, limit: {}", currentConstraint, pollingLimit);
-                    Collection<StockEvent> events = serviceAdapter.getEventRange(currentConstraint, pollingLimit);
-                    EventHandler<StockEvent> eventHandler = pollingConfig.getEventHandler();
-                    StockEvent lastEvent = null;
-                    for (StockEvent event : events) {
-                        if (!running) {
-                            break;
-                        }
-                        lastEvent = event;
-                        try {
-                            if (!pollingConfig.getEventFilter().accept(event)) {
-                                log.trace("Event not accepted: {}", event);
-                                continue;
-                            }
-                            log.trace("Event accepted: {}", event);
-                            eventHandler.handleEvent(event, subscriptionKey);
-                        } catch (Throwable t) {
-                            if (markIfInterrupted(t)) {
-                                log.error("Event handling was interrupted, [break]");
-                                break;
-                            } else {
-                                log.error("Error during event handling event: [" + event + "]", t);
-                            }
-                        }
-                    }
-                    if (events.size() < pollingLimit) {
-                        if (rangeWalker.getWalkingRange().isToDefined()) {
-                            exhausted = true;
+                        log.debug("Range is not initialized, init range");
+                        rangeWalker = initRange(pollingConfig.getEventFilter().getEventConstraint());
+                        if (rangeWalker == null) {
+                            log.debug("Range walker is not initialized, pause");
+                            return;
                         } else {
-                            suspended = true;
+                            log.debug("Range initialized: {}", rangeWalker);
                         }
                     }
-                    if (lastEvent != null) {
-                        final StockEvent tmpLastEvent = lastEvent;
-                        rangeWalker.moveRange((walker, boundInclusive) -> {
-                            Comparable val;
-                            if (walker instanceof IdRangeWalker) {
-                                val = ValuesExtractor.getEventId(tmpLastEvent);
-                            } else {
-                                val = Instant.from(ValuesExtractor.getCreatedAt(tmpLastEvent));
+
+                    while (running && !exhausted && !suspended && !Thread.currentThread().isInterrupted()) {
+                        if (rangeWalker.isRangeOver()) {
+                            log.debug("Range is over: {}", rangeWalker);
+                            exhausted = true;
+                            continue;
+                        }
+
+                        EventConstraint currentConstraint = new EventConstraint(rangeWalker.getWalkingRange());
+
+                        log.debug("Tying to get event range, constraint: {}, limit: {}", currentConstraint, pollingLimit);
+                        Collection<StockEvent> events = serviceAdapter.getEventRange(currentConstraint, pollingLimit);
+                        EventHandler<StockEvent> eventHandler = pollingConfig.getEventHandler();
+                        StockEvent lastEvent = null;
+                        for (StockEvent event : events) {
+                            if (!running) {
+                                break;
                             }
-                            return new Pair(val, false);
-                        });
-                        log.debug("Range moved to: {}", rangeWalker);
+                            lastEvent = event;
+                            try {
+                                if (!pollingConfig.getEventFilter().accept(event)) {
+                                    log.trace("Event not accepted: {}", event);
+                                    continue;
+                                }
+                                log.trace("Event accepted: {}", event);
+                                eventHandler.handleEvent(event, subscriptionKey);
+                            } catch (Throwable t) {
+                                if (markIfInterrupted(t)) {
+                                    log.error("Event handling was interrupted, [break]");
+                                    break;
+                                } else {
+                                    log.error("Error during event handling event: [" + event + "]", t);
+                                }
+                            }
+                        }
+                        if (events.size() < pollingLimit) {
+                            if (rangeWalker.getWalkingRange().isToDefined()) {
+                                exhausted = true;
+                            } else {
+                                suspended = true;
+                            }
+                        }
+                        if (lastEvent != null) {
+                            final StockEvent tmpLastEvent = lastEvent;
+                            rangeWalker.moveRange((walker, boundInclusive) -> {
+                                Comparable val;
+                                if (walker instanceof IdRangeWalker) {
+                                    val = ValuesExtractor.getEventId(tmpLastEvent);
+                                } else {
+                                    val = Instant.from(ValuesExtractor.getCreatedAt(tmpLastEvent));
+                                }
+                                return new Pair(val, false);
+                            });
+                            log.debug("Range moved to: {}", rangeWalker);
+                        }
+                    }
+
+
+                } catch (ServiceException e) {
+                    if (e.getCause() instanceof DatasetTooBig) {
+                        DatasetTooBig dtbEx = (DatasetTooBig) e.getCause();
+                        log.info("Current query size: '{}' is too big, new size is: '{}'", pollingLimit, dtbEx.getLimit());
+                        //we shouldn't get into DatasetTooBig often so we can afford waiting an iteration to continue. This can be changed later.
+                        pollingLimit = dtbEx.getLimit();
+                    } else if (markIfInterrupted(e.getCause())) {
+                        log.info("Task interrupted [break]");
+                        return;
+                    } else  {
+                        log.warn("Failed to execute request to repository service, caused by: {}", e.getMessage());
+
+                        try {
+                            ErrorActionType actionType = pollingConfig.getErrorHandler().handleError(subscriptionKey, e);
+                            switch (actionType) {
+                                case RETRY:
+                                    log.warn("Retry request after error");
+                                    return;
+                                case INTERRUPT:
+                                    log.warn("Interrupt request after error");
+                                    exhausted = true;
+                                    break;
+                                default:
+                                    throw new IllegalStateException("Unknown error action: " + actionType);
+                            }
+                        } catch (Throwable t) {
+                            log.error("Error during error handling", t);
+                            markIfInterrupted(t);
+                        }
                     }
                 }
 
-
-            } catch (ServiceException e) {
-                if (e.getCause() instanceof DatasetTooBig) {
-                    DatasetTooBig dtbEx = (DatasetTooBig) e.getCause();
-                    log.info("Current query size: '{}' is too big, new size is: '{}'", pollingLimit, dtbEx.getLimit());
-                    //we shouldn't get into DatasetTooBig often so we can afford waiting an iteration to continue. This can be changed later.
-                    pollingLimit = dtbEx.getLimit();
-                } else if (markIfInterrupted(e.getCause())) {
-                    log.info("Task interrupted [break]");
-                    return;
-                } else  {
-                    log.warn("Failed to execute request to repository service, caused by: {}", e.getMessage());
-
+                if (exhausted) {
+                    log.debug("Subscription exhausted");
+                    poller.directRemovePolling(subscriptionKey);
                     try {
-                        ErrorActionType actionType = pollingConfig.getErrorHandler().handleError(subscriptionKey, e);
-                        switch (actionType) {
-                            case RETRY:
-                                log.warn("Retry request after error");
-                                return;
-                            case INTERRUPT:
-                                log.warn("Interrupt request after error");
-                                exhausted = true;
-                                break;
-                            default:
-                                throw new IllegalStateException("Unknown error action: " + actionType);
-                        }
+                        pollingConfig.getEventHandler().handleNoMoreElements(subscriptionKey);
                     } catch (Throwable t) {
-                        log.error("Error during error handling", t);
+                        log.error("Error during event handling [exhausted]", t);
                         markIfInterrupted(t);
                     }
                 }
-            }
-
-            if (exhausted) {
-                log.debug("Subscription exhausted");
-                poller.directRemovePolling(subscriptionKey);
-                try {
-                    pollingConfig.getEventHandler().handleNoMoreElements(subscriptionKey);
-                } catch (Throwable t) {
-                    log.error("Error during event handling [exhausted]", t);
-                    markIfInterrupted(t);
+            } catch (Throwable t) {
+                log.error("Error during poll processing, task is broken", t);
+                if (!markIfInterrupted(t)) {
+                    throw new RuntimeException("Task is broken", t);
                 }
+            } finally {
+                LogSupport.removeSubscriptionKey();
             }
-        } catch (Throwable t) {
-            log.error("Error during poll processing, task is broken", t);
-            if (!markIfInterrupted(t)) {
-                throw new RuntimeException("Task is broken", t);
-            }
-        } finally {
-            LogSupport.removeSubscriptionKey();
-        }
+        }).run();
     }
 
     void stop() {
